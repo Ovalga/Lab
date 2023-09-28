6f62f60d41595fd86e9573960fc8ecf9020e17c0
ShkurkinaM <skrknmaria@gmail.com>
2023-09-28 09:19:45 +0400

    task 1.5 create class CompositeFunction

--- src/main/java/org/example/functions/CompositeFunction.java
+++ src/main/java/org/example/functions/CompositeFunction.java
@@ -5,11 +5,12 @@ public class CompositeFunction implements MathFunction {
     private MathFunction firstFunction;
     private MathFunction secondFunction;
 
-    public CompositeFunction (MathFunction firstFunction, MathFunction secondFunction) {
-        this.firstFunction= firstFunction;
+    public CompositeFunction(MathFunction firstFunction, MathFunction secondFunction) {
+        this.firstFunction = firstFunction;
         this.secondFunction = secondFunction;
     }
+
     public double apply(double x) {
-        return x;
+        return secondFunction.apply(firstFunction.apply(x));
     }
 }

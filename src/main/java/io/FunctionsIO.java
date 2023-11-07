947eb3dee1acc54d6775d40d0aa6b4966503b07b
Adiyanova <aolga004@mail.ru>
2023-11-07 23:13:49 +0400

    Lab5 task 3...

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -3,21 +3,29 @@ package io;
 import org.example.functions.Point;
 import org.example.functions.TabulatedFunction;
 
-import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
     }
+
     public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
         DataOutputStream out = new DataOutputStream(outputStream);
         out.writeInt(function.getCount());
-        for(Point point : function){
+        for (Point point : function) {
             out.writeDouble(point.x);
             out.writeDouble(point.y);
         }
         out.flush();
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+        printWriter.flush();
     }
 }

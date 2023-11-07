77d558ada516b0a9108a82df8bb6a2966f05ada4
ShkurkinaM <skrknmaria@gmail.com>
2023-11-07 18:27:07 +0400

    task 2 lab5

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -1,7 +1,23 @@
 package io;
 
+import org.example.functions.Point;
+import org.example.functions.TabulatedFunction;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream out = new DataOutputStream(outputStream);
+        out.writeInt(function.getCount());
+        for(Point point : function){
+            out.writeDouble(point.x);
+            out.writeDouble(point.y);
+        }
+        out.flush();
     }
 }
---
+++ src/main/java/io/TabulatedFunctionFileOutputStream.java
@@ -0,0 +1,27 @@
+package io;
+
+import org.example.functions.ArrayTabulatedFunction;
+import org.example.functions.LinkedListTabulatedFunction;
+import org.example.functions.TabulatedFunction;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TabulatedFunctionFileOutputStream {
+    public static void main(String[] args) {
+        try (BufferedOutputStream arrayFileOutput = new BufferedOutputStream(new FileOutputStream("output/array function.bin"));
+             BufferedOutputStream linkedListFileOutput = new BufferedOutputStream(new FileOutputStream("output/linked list function.bin"))) {
+            double[] xValue = {1, 3, 5, 7};
+            double[] yValue = {2, 4, 6, 8};
+            TabulatedFunction arrayFunction = new ArrayTabulatedFunction(xValue, yValue);
+            TabulatedFunction linkedListFunction = new LinkedListTabulatedFunction(xValue, yValue);
+
+            FunctionsIO.writeTabulatedFunction(arrayFileOutput, arrayFunction);
+            FunctionsIO.writeTabulatedFunction(linkedListFileOutput, linkedListFunction);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

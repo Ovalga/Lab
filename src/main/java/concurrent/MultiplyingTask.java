8563322e279ebe323282496ebd15a692f77e6348
Adiyanova <aolga004@mail.ru>
2023-11-18 13:13:14 +0400

    Lab 6 task 1.1

---
+++ src/main/java/concurrent/MultiplyingTask.java
@@ -0,0 +1,20 @@
+package concurrent;
+
+import org.example.functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable {
+    private TabulatedFunction tabulatedFunction;
+
+    public MultiplyingTask(TabulatedFunction tabulatedFunction) {
+        this.tabulatedFunction = tabulatedFunction;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < tabulatedFunction.getCount(); i++) {
+            tabulatedFunction.setY(i, tabulatedFunction.getY(i) * 2);
+        }
+         String thread = Thread.currentThread().getName();
+        System.out.println("thread " + thread  + "has completed the task.");
+    }
+}

9e157ec73b1cf5a1dd7f9ae461f113f634f9629a
ShkurkinaM <skrknmaria@gmail.com>
2023-11-19 14:46:52 +0400

    task 2 lab 6

--- src/main/java/concurrent/MultiplyingTaskExecutor.java
+++ src/main/java/concurrent/MultiplyingTaskExecutor.java
@@ -13,14 +13,14 @@ public class MultiplyingTaskExecutor {
     public static void main(String[] args) throws InterruptedException {
         TabulatedFunction tabulatedFunction = new LinkedListTabulatedFunction(new UnitFunction(), 1, 1000, 1000);
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-        CountDownLatch countDownLatch = new CountDownLatch(10);
+
 
         for (int i = 0; i < 10; i++) {
             Runnable task = new MultiplyingTask(tabulatedFunction);
             executorService.execute(task);
         }
 
-        countDownLatch.await();
+        Thread.sleep(1000);
         executorService.shutdown();
 
         System.out.println(tabulatedFunction);

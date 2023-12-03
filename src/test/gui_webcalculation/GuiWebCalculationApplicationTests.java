0f021852f0dd9216096dabbd3aeae864c154101a
ShkurkinaM <skrknmaria@gmail.com>
2023-12-03 18:47:13 +0400

    lab 7

---
+++ src/test/gui_webcalculation/GuiWebCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gui_webcalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiWebCalculationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
